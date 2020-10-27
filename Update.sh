e3c787d6cd660bb46f4b3ca5cfe7d577a85a2c76
CrashCortez <46716213+CrashCortez@users.noreply.github.com>
2020-10-26 19:22:59 -0700

    Update Update.sh

--- Update.sh
+++ Update.sh
@@ -6,11 +6,18 @@ git clone https://github.com/CrashCortez/konami
 cd konami
 sudo chmod 777 Update.sh
 sudo chmod a+x Update.sh
+cd boxart
+sudo chmod 777 *.sh
+sudo chmod a+x *.sh
+cd .full
+sudo chmod 777 *.sh
+sudo chmod a+x *.sh
+cd ..
 cd full
 sudo chmod 777 *.sh
 sudo chmod a+x *.sh
 cd ..
-cd boxart
+cd .hacks
 sudo chmod 777 *.sh
 sudo chmod a+x *.sh
 cd ..

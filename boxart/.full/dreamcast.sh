4db5f73189c92d2babf2126a317b025eb08f85eb
CrashCortez <46716213+CrashCortez@users.noreply.github.com>
2020-11-08 12:22:50 -0800

    Create dreamcast.sh

---
+++ boxart/.full/dreamcast.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd
+mkdir test
+cd test
+wget -c https://archive.org/download/mb_unified_rompacks_202010/dreamcast_unified_rompack.7z -q --show-progress
+7z x -y -aoa /home/pi/test/dreamcast_unified_rompack.7z
+sudo mv dreamcast /home/pi/RetroPie/roms
+sudo rm "dreamcast_unified_rompack.7z"
+cd ..
+sudo rm -R test
+touch /tmp/es-restart && kill -15 $(pidof emulationstation)

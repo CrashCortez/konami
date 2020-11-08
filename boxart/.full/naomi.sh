e88ea83e20bce8c018ecd1c4d0fdd240b9d367f6
CrashCortez <46716213+CrashCortez@users.noreply.github.com>
2020-11-08 15:07:13 -0800

    Update and rename noami.sh to naomi.sh

---
+++ boxart/.full/naomi.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd
+mkdir test
+cd test
+wget -c https://archive.org/download/mb_unified_rompacks_202010/naomi_unified_rompack.7z -q --show-progress
+7z x -y -aoa /home/pi/test/naomi_unified_rompack.7z
+sudo mv naomi /home/pi/RetroPie/roms
+sudo rm "naomi_unified_rompack.7z"
+cd ..
+sudo rm -R test
+touch /tmp/es-restart && kill -15 $(pidof emulationstation)
